45fe1272b4b5db59cdd139f5ec453c42a6bae66e
gordo240620 <manuelpacheco031105@gmail.com>
2025-03-21 11:42:40 -0600

    practica 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NuevoComponenteComponent } from './nuevo-componente/nuevo-componente.component';
+
+import { ComponenteInlineComponent } from './componente-inline/componente-inline.component';
+
+import { PadreComponent } from './padre/padre.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+   NuevoComponenteComponent,
+   ComponenteInlineComponent,
+   PadreComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/padre/hijo/hijo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PadreComponent } from '../padre.component';
+
+@Component({
+  selector: 'app-hijo',
+  imports: [PadreComponent],
+  templateUrl: './hijo.component.html',
+  styleUrl: './hijo.component.css'
+})
+export class HijoComponent {
+
+}
