ab50c7d35ddcfdf17e085545a4ef4d8968318fb4
gordo240620 <manuelpacheco031105@gmail.com>
2025-03-21 17:24:03 -0600

    Practica 18

---
+++ src/app/agregar-tarea/agregar-tarea.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-tarea',
+  imports: [],
+  templateUrl: './agregar-tarea.component.html',
+  styleUrl: './agregar-tarea.component.css'
+})
+export class AgregarTareaComponent {
+  tarea: string = '';
+  agregarTarea(nuevaTarea: string): void {
+  this.tarea = nuevaTarea;
+  }
+}
