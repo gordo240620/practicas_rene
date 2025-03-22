b025fa8ce047a3c0456304b7ce7b18004a757d2e
gordo240620 <manuelpacheco031105@gmail.com>
2025-03-21 18:16:26 -0600

    Practica 23

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FormularioComponent } from "../formulario/formulario.component";
+import { ResultadoComponent } from "../resultado/resultado.component";
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [FormsModule, FormularioComponent, ResultadoComponent],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+  resultado: number | null = null;
+
+  procesarResultado(resultado: number) {
+  this.resultado = resultado;
+  }
+}
